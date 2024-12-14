df08878f0493fa3ba1b8cd63ae59c2f64d81045d
Praanshu Grover <praanshugrover60@gmail.com>
2024-12-14 13:26:11 +0530

    feat: Implement routing with react-router-dom and add Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+
 import "./index.css";
 
 const App = () => {
-  return <StrictMode>Hello World!</StrictMode>;
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "text-default": "#caf0f8",
+        "rich-black-300": "#001A2C",
+        "rich-black-500": "#001523",
+        "rich-black-700": "#00111C",
+        "purssian-blue-200": "#00406C",
+        "purssian-blue-300": "#003A61",
+        "purssian-blue-400": "#003356",
+        "purssian-blue-500": "#002E4E",
+        "purssian-blue-600": "#002945",
+        "purssian-blue-700": "#00253E",
+        "purssian-blue-800": "#002137",
+        "focus-border": "#0073e6",
+      },
+    },
   },
   plugins: [],
-}
-
+};
