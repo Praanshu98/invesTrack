93950f2af4110e13f26acaa0c64ab4b78b6ab00c
Praanshu Grover <praanshugrover60@gmail.com>
2024-12-18 17:10:30 +0530

    refactor: refactor customButton, validateAndSignup, validation
    1. change customButton to user button tag
    2. remove repeated code of validateAndSignup from validation files
    3. add password validation in validateAndSignup, adds try-catch block for error handling

--- frontend/src/components/customButtons.jsx
+++ frontend/src/components/customButtons.jsx
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
-const CustomButton = ({ customValue, customType, ...props }) => {
+const CustomButton = ({ customValue, customType, className, ...props }) => {
   return (
-    <input
-      type={customType || "button"}
-      value={customValue || "Button"}
-      className="m-2 p-2 bg-blue-500 text-white rounded border-2 border-blue-500 focus:border-2 focus:border-red-500 focus:outline-none focus:ring-0"
+    <button
+      type={customType || "submit"}
+      className={
+        "m-2 p-2 bg-blue-500 rounded border-2 border-blue-500 focus:border-2 focus:border-red-500 focus:outline-none focus:ring-0 " +
+        className
+      }
       {...props}
-    />
+    >
+      {customValue}
+    </button>
   );
 };
 
--- frontend/src/utils/validateAndSignup.js
+++ frontend/src/utils/validateAndSignup.js
@@ -1,43 +1,60 @@
 import signUp from "../utils/signUp";
 import login from "../utils/login";
 
+import { validatePassword } from "./validation";
+
 const validateAndSignUp = async (event, setUser, navigate) => {
-  event.preventDefault();
+  try {
+    event.preventDefault();
+    document.getElementById("signup-error").classList.add("hidden");
 
-  const firstName = event.target.firstName.value;
-  const lastName = event.target.lastName.value;
-  const email = event.target.email.value;
-  const password = event.target.password.value;
+    const firstName = event.target.firstName.value;
+    const lastName = event.target.lastName.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-  // Try to create a user
-  const response = await signUp({ firstName, lastName, email, password });
+    // Check if the password is valid
+    if (!validatePassword(password)) {
+      throw new Error(
+        "Password must be between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter, and one special character."
+      );
+    }
 
-  if (response.status === 400) {
-    // If there was an error, show the error
-    const error = await response.json();
+    // Try to create a user
+    const response = await signUp({ firstName, lastName, email, password });
+
+    if (response.status === 400) {
+      // If there was an error, show the error
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+
+    if (response.status === 201) {
+      // If the user was created successfully, log them in
+      const loginUserResponse = await login({ email, password });
+
+      if (loginUserResponse.status === 200) {
+        // If the user was logged in successfully, set the user in the context
+        const user = await loginUserResponse.json();
+        setUser(user.user);
+
+        // Redirect to the dashboard
+        navigate("/dashboard");
+      }
+
+      if (
+        loginUserResponse.status === 400 ||
+        loginUserResponse.status === 500
+      ) {
+        // If there was an error, show the error
+        const error = await loginUserResponse.json();
+        throw new Error(error.message);
+      }
+    }
+  } catch (error) {
+    console.error("Error in validateAndSignUp", error);
     document.getElementById("signup-error").classList.remove("hidden");
     document.getElementById("signup-error").textContent = error.message;
-  }
-
-  if (response.status === 201) {
-    // If the user was created successfully, log them in
-    const loginUserResponse = await login({ email, password });
-
-    if (loginUserResponse.status === 200) {
-      // If the user was logged in successfully, set the user in the context
-      const user = await loginUserResponse.json();
-      setUser(user.user);
-
-      // Redirect to the dashboard
-      navigate("/dashboard");
-    }
-
-    if (loginUserResponse.status === 400 || loginUserResponse.status === 500) {
-      // If there was an error, show the error
-      const error = await loginUserResponse.json();
-      document.getElementById("signup-error").classList.remove("hidden");
-      document.getElementById("signup-error").textContent = error.message;
-    }
   }
 };
 
