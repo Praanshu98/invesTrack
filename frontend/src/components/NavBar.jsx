98a23e80745bba47a02eb4061b2b7529b369d3d2
Praanshu Grover <praanshugrover60@gmail.com>
2024-12-20 16:41:00 +0530

    refactor: update NavBar to conditionally render links based on user login status

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,36 @@
 import { Link } from "react-router-dom";
+import { useUserContext } from "../context/userContext";
 
 const NavBar = () => {
+  const { user } = useUserContext();
+
   return (
     <nav>
-      <ul className="flex flex-row m-2 gap-5 md:gap-10 justify-end mt-8 mr-8">
-        <li>
-          <Link to="/"> Home </Link>
-        </li>
-        <li>
-          <Link to="/login"> Login / Signup </Link>
-        </li>
+      <ul className="flex m-2 md:gap-10 mt-8 mr-8 justify-between">
+        <div>
+          {/* If use is logged in show dashboard else show the landing page. */}
+          {!user.isLoggedIn ? (
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/dashboard"> Dashboard </Link>
+            </li>
+          )}
+        </div>
+        {/* If user is logged in show logout else signup or login. */}
+        <div className="flex gap-5">
+          {!user.isLoggedIn ? (
+            <li>
+              <Link to="/login"> Login / Signup </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/logout"> Logout </Link>
+            </li>
+          )}
+        </div>
       </ul>
     </nav>
   );
