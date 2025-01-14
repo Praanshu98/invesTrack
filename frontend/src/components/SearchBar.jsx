b8f84a7ab8c50dc6e853b9ef46a25c4ee7108c03
Praanshu Grover <praanshugrover60@gmail.com>
2025-01-15 00:04:12 +0530

    fix: search result rendering at initial load.
    1. Fixes issue with search result div getting rendered at initial page
       loading.
    2. Reduces search term result trigger from 4 letters to 3 letters.

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import getListOfAllMutualFunds from "../utils/listAllMutualfunds";
 
 const SearchBar = () => {
-  const [mutualFundList, setMutualFundList] = useState(null);
+  const [mutualFundList, setMutualFundList] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
@@ -14,38 +14,33 @@ const SearchBar = () => {
     getAllMutualFunds();
   }, []);
 
-  const filteredMutualFunds = mutualFundList?.filter((mutualFund) => {
+  const filteredMutualFunds = mutualFundList.filter((mutualFund) => {
     return (
-      searchTerm.length > 3 &&
+      searchTerm.length > 2 &&
       mutualFund.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
   });
-
-  console.log("filteredMutualFunds => ", filteredMutualFunds);
 
   return (
     <div className="w-3/4 md:w-4/5">
       {/* Search Input */}
       <input
         type="text"
-        className={`focus:shadow-outline h-16 w-full rounded-xl border-none bg-purssian-blue-400 px-5 text-xl shadow-none outline-none placeholder:font-light placeholder:text-slate-400 focus:outline-none focus:outline-transparent focus:ring-transparent md:text-2xl ${filteredMutualFunds?.length > 0 ? "rounded-b-none" : ""}`}
+        className={`focus:shadow-outline h-16 w-full rounded-xl border-none bg-purssian-blue-400 px-5 text-xl shadow-none outline-none placeholder:font-light placeholder:text-slate-400 focus:outline-none focus:outline-transparent focus:ring-transparent md:text-2xl ${filteredMutualFunds?.length !== 0 ? "rounded-b-none" : ""}`}
         placeholder="Search Mutual Funds"
         onChange={(event) => {
           setSearchTerm(event.target.value);
-          console.log(searchTerm);
         }}
       />
       {/* Search Result */}
-      {filteredMutualFunds?.length != 0 ? (
-        <div className="truncate rounded-b-xl bg-purssian-blue-200 text-slate-100">
-          <ul className="max-h-52 overflow-y-auto py-2 text-lg font-light">
-            {filteredMutualFunds?.map((mutualFund) => (
-              <div key={mutualFund.isin} className="mx-5 truncate py-1">
-                <li>{mutualFund.name}</li>
-              </div>
-            ))}
-          </ul>
-        </div>
+      {filteredMutualFunds.length !== 0 ? (
+        <ul className="max-h-52 overflow-y-auto rounded-b-xl bg-purssian-blue-200 py-2 text-lg font-light text-slate-100">
+          {filteredMutualFunds.map((mutualFund) => (
+            <li key={mutualFund.isin} className="mx-5 truncate py-1">
+              {mutualFund.name}
+            </li>
+          ))}
+        </ul>
       ) : (
         ""
       )}
