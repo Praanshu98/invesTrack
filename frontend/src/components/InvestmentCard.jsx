bc0bb121937121f4a9afdefd36dabbf3c2c98b7c
Praanshu Grover <praanshugrover60@gmail.com>
2025-01-17 20:56:10 +0530

    feat: adds initial template for Investment Card.

---
+++ frontend/src/components/InvestmentCard.jsx
@@ -0,0 +1,48 @@
+const InvestmentCard = ({ name, units, nav, value, classNames }) => {
+  name = "HDFC Nifty 50 Index Fund - Direct Plan - Growth Plan";
+  units = 123000;
+  nav = 10;
+  value = 11000000;
+
+  function formatValue(value, currency = false) {
+    let postfix = null;
+    if (value / 10000000 >= 1) {
+      value = value / 10000000;
+      postfix = " Crores";
+    }
+    if (value / 100000 >= 1) {
+      value = value / 100000;
+      postfix = " Lakhs";
+    }
+    return `${new Intl.NumberFormat("en-IN", {
+      // Conditionally providing values
+      ...(currency && { currency: "INR", style: "currency" }),
+      maximumFractionDigits: postfix ? 2 : 0,
+      roundingMode: "trunc",
+      trailingZeroDisplay: "stripIfInteger",
+    }).format(value)} ${postfix ? postfix : ""}`;
+  }
+
+  return (
+    <div
+      className={`flex h-32 w-96 flex-col justify-between text-pretty rounded border-2 border-purssian-blue-200 bg-purssian-blue-700 p-3 ${classNames}`}
+    >
+      <p className=""> {name} </p>
+      <div className="flex justify-between">
+        <p className="place-content-center text-4xl">
+          {formatValue(value, true)}
+        </p>
+        <div className="flex flex-col justify-between text-xs">
+          <p>
+            Units: <span className="text-base"> {formatValue(units)} </span>
+          </p>
+          <p>
+            Nav: <span className="text-base"> {nav} </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InvestmentCard;
