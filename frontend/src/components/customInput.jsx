9c0a19459f09353aac872fb9da9a46f700062ec5
Praanshu Grover <praanshugrover60@gmail.com>
2025-01-20 00:05:53 +0530

    refactor: fix classNames in customInput component

--- frontend/src/components/customInput.jsx
+++ frontend/src/components/customInput.jsx
@@ -1,17 +1,9 @@
-const CustomInput = ({
-  placeholderText,
-  inputType,
-  classNameText,
-  ...props
-}) => {
+const CustomInput = ({ placeholderText, inputType, classNames, ...props }) => {
   return (
     <input
       type={inputType || "text"}
       placeholder={placeholderText || "placeholder text"}
-      className={
-        "m-2 min-w-40 rounded-lg border-2 border-purssian-blue-200 bg-purssian-blue-800 p-2 placeholder-slate-500 focus:border-red-500 focus:outline-none focus:ring-0 " +
-        classNameText
-      }
+      className={`m-2 min-w-40 rounded-lg border-2 border-purssian-blue-200 bg-purssian-blue-800 p-2 placeholder-slate-500 focus:border-red-500 focus:outline-none focus:ring-0 ${classNames}`}
       {...props}
     />
   );
